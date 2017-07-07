80d9b4c27a3371e83a8a695fe9becdc7d8ae3af1
Corpelijn,Bas B.J <b.corpelijn@student.fontys.nl>
2017-07-07 13:31:30 +0200

    Initial commit PIX-13 phantom

---
+++ QCPhantom/PIX-13/PIX13Analyser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PIX13
+{
+    class PIX13Analyser
+{
+    #region "Fields"
+
+
+
+    #endregion
+
+    #region "Constructors"
+
+
+
+    #endregion
+
+    #region "Properties"
+
+
+
+    #endregion
+
+    #region "Methods"
+
+
+
+    #endregion
+
+    #region "Abstract/Virtual Methods"
+
+
+
+    #endregion
+
+    #region "Inherited Methods"
+
+
+
+    #endregion
+
+    #region "Static Methods"
+
+
+
+    #endregion
+
+    #region "Operators"
+
+
+
+    #endregion
+}
+}
