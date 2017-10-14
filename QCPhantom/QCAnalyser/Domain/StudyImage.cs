4daa92f68d00b4407c6fe473cfa5ad91d089d248
Corpelijn,Bas B.J <b.corpelijn@student.fontys.nl>
2017-10-14 15:44:58 +0200

    Fixed a bug in displaying the date/time from study

---
+++ QCPhantom/QCAnalyser/Domain/StudyImage.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace QCAnalyser.Domain
+{
+    /// <summary>
+    /// A public object containing information about the image in a study
+    /// </summary>
+    public class StudyImage
+    {
+        #region "Fields"
+
+        private long id;
+        private string imageUID;
+        private ImageStatus status;
+        private Machine machine;
+        private Detector detector;
+        private string analyseMethod;
+        private DateTime analyseTime;
+
+        #endregion
+
+        #region "Constructors"
+
+        public StudyImage(int id, string imageUID, ImageStatus status, Machine machine, Detector detector, string analyseMethod, DateTime analyseTime)
+        {
+            this.id = id;
+            this.imageUID = imageUID;
+            this.status = status;
+            this.machine = machine;
+            this.detector = detector;
+            this.analyseMethod = analyseMethod;
+#warning Needs to be changed
+            this.analyseTime = DateTime.Now;
+        }
+
+        #endregion
+
+        #region "Properties"
+
+        /// <summary>
+        /// Gets the uniquq id of the image in the database
+        /// </summary>
+        public long Id
+        {
+            get { return this.id; }
+        }
+
+        /// <summary>
+        /// Gets the ImageUID of the image and study
+        /// </summary>
+        public string ImageUID
+        {
+            get { return this.imageUID; }
+        }
+
+        /// <summary>
+        /// Gets the status of the image after analysing
+        /// </summary>
+        public ImageStatus Status
+        {
+            get { return this.status; }
+        }
+
+        /// <summary>
+        /// Gets the machine the image was taken on
+        /// </summary>
+        public Machine Machine
+        {
+            get { return this.machine; }
+        }
+
+        /// <summary>
+        /// Gets the detector the image was taken on
+        /// </summary>
+        public Detector Detector
+        {
+            get { return this.detector; }
+        }
+
+        /// <summary>
+        /// Gets the method used to analyse the image
+        /// </summary>
+        public string AnalyseMethod
+        {
+            get { return this.analyseMethod; }
+        }
+
+        /// <summary>
+        /// Gets the date and time whe
+        /// </summary>
+        public DateTime AnalyseTime
+        {
+            get { return this.analyseTime; }
+        }
+
+        #endregion
+
+        #region "Methods"
+
+
+
+        #endregion
+
+        #region "Abstract/Virtual Methods"
+
+
+
+        #endregion
+
+        #region "Inherited Methods"
+
+
+
+        #endregion
+
+        #region "Static Methods"
+
+
+
+        #endregion
+
+        #region "Operators"
+
+
+
+        #endregion
+    }
+}
--- QCPhantom/QCPhantom/Controllers/ResultsController.cs
+++ QCPhantom/QCPhantom/Controllers/ResultsController.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using QCPhantom.Domain.Other;
+using QCAnalyser.Domain;
 
 namespace QCPhantom.Controllers
 {
     public class ResultsController : Controller
     {
+        private static Random rand = new Random();
+        private static int staticid = -1;
+
         [HttpGet]
         public IActionResult StudyResult(int id)
         {
@@ -22,9 +26,35 @@ namespace QCPhantom.Controllers
         }
 
         [HttpGet]
-        public IActionResult FilterResults()
+        public IActionResult FilterResults(string orderby, string orderdirection, int index, int length)
         {
-            return Json(new { name = "value" });
+            object newData = new { url = "" };
+            //if (staticid % 4 != 0)
+            //    newData = new { url = "/Results/FilterResults?orderby=analysedate&orderdirection=asc&index=0&length=1" };
+
+            return Json(new { studies = new object[] { Study.GenerateRandomStudy() }, data = newData });
+        }
+
+        private object GetImage()
+        {
+            return new { imageUID = "9234.9834.9849283.03498059348502.456", state = "In Orde", machine = "QC Light Kamer 5", detector = "Detector 4" };
+        }
+
+        private object GetStudy()
+        {
+            int r = rand.Next(1, 10);
+            object[] images = new object[r];
+            for (int i = 0; i < r; i++)
+            {
+                images[i] = GetImage();
+            }
+
+            return new { id = staticID, dateTime = "17 juni 2017 17:48", pacsAccession = "51234854", studyUID = "9234.9834.9849283.03498059348502.456", images = images };
+        }
+
+        public int staticID
+        {
+            get { staticid++; return staticid; }
         }
     }
 }
